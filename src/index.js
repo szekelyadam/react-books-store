24d2913463dd434c66553168d9d9859e5c49e503
szekelyadam <szekelyadam94@gmail.com>
2016-12-28 01:40:59 +0100

    Provide store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { router, browserHistory } from 'react-router';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import configureStore from './store/configureStore'
+
+const store = configureStore();
+
 render(
-  <Router routes={routes} history={browserHistory} />,
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
   document.getElementById('app')
 );
