0d163f090f1e730a04a43706719284e1bba53182
szekelyadam <szekelyadam94@gmail.com>
2016-12-28 01:31:52 +0100

    Combine reducers (it's only one but I call it progress)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
 // Set up your root reducer here...
- import { combineReducers } from 'redux';
- export default combineReducers;
+import { combineReducers } from 'redux';
+import books from './bookReducers';
+
+export default combineReducers ({
+  books: books,
+});
